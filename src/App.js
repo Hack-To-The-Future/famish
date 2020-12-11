ff4a0a43132c1975dbdcd6e8cb763331a88f92dd
Nick Martin <nicholas.martin@tharsus.co.uk>
2020-12-11 11:11:58 +0000

    learning phaser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
+import Phaser from 'phaser';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  useEffect(() => {
+    var config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 },
+          debug: false
+        }
+      },
+      scene: {
+          preload: preload,
+          create: create,
+          update: update
+      }
+  };
+  
+  var game = new Phaser.Game(config);
+  
+  function preload ()
+  {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+  }
+
+  var platforms;
+  var player;
+  var cursors;
+  var stars;
+  var scale = 1;
+  const collectStar = (player, star) => {
+    star.disableBody(true, true);
+
+    console.log(player);
+    scale *= 1.5;
+    player.setScale(scale);
+  }
+
+  function create ()
+  {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
+
+    platforms = this.physics.add.staticGroup();
+
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+  
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+      child.setCollideWorldBounds(true);
+    });
+    
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+  }
+  
+  function update ()
+  {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+      player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+      player.anims.play('right', true);
+    }
+    else {
+      player.setVelocityX(0);
+      player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown) {
+      player.setVelocityY(-160);
+      player.anims.play('turn', true);
+    }
+    else if (cursors.down.isDown) {
+      player.setVelocityY(160);
+      player.anims.play('turn', true);
+    }
+    else {
+      //player.setVelocityY(0);
+      player.anims.play('turn');
+    }
+  }
+
+  },[]);
+
+  return <></>
 }
 
 export default App;
